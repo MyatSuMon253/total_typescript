7c0b1f58dc49723f0c602b043c71863e0aaa9a73
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-09-17 00:34:41 +0630

    chore: comment error

--- solving_typescript_errors/03-possibly_null_or_undefined.ts
+++ solving_typescript_errors/03-possibly_null_or_undefined.ts
@@ -3,7 +3,7 @@ const searchParams = new URLSearchParams(window.location.search);
 const id = searchParams.get("id");
 
 // error
-console.log(id.toUpperCase());
+// console.log(id.toUpperCase());
 
 // solution 1 optional chain
 console.log(id?.toUpperCase());
@@ -17,12 +17,12 @@ if (id) {
 }
 
 // solution 4 type narrowing by typeof check
-if (typeof id === 'string') {
+if (typeof id === "string") {
   console.log(id.toUpperCase());
 }
 
 // solution 5 throw error
 if (!id) {
- throw new Error("Id not found")
+  throw new Error("Id not found");
 }
 console.log(id.toUpperCase());
