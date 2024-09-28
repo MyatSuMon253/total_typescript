bf2606f3a767981d9306d5d159c9fd09b21c2343
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-09-28 08:48:58 +0630

    fix: could not find a declaration file

---
+++ solving_typescript_errors/10-could_not_find_a_declaration_file.ts
@@ -0,0 +1,8 @@
+import Diff from "diff";
+
+console.log(Diff);
+
+/* problem => Could not find a declaration file for module 'diff'.
+solution add @types/diff community declaration file for this library
+npm i --save-dev @types/diff
+*/
